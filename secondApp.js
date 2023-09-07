e15a242a6de2e3c335053cb27ffe8389ba284b5e
AmanRajVerma10 <verma.aman464@gmail.com>
2023-09-07 19:10:30 +0530

    secondPush

---
+++ secondApp.js
@@ -0,0 +1,35 @@
+//console.log(document.title);
+//document.title= 123;
+//console.log(document.title);
+//console.log(document.links);
+//console.log(document.forms[0]);
+
+//console.log(document.getElementById('header-title'));
+var x=document.getElementById('header-title');
+//x.textContent='Hazard';
+//x.innerText='Aman';
+//console.log(x.innerText);
+//x.innerHTML='<h2>Heyyy</h2>';
+//x.style.borderBottom= 'solid 3px #000';
+var y=document.getElementById('main-header');
+y.style.borderBottom= 'solid 3px #000';
+// x.style.color="#FF0000";
+//var a=document.getElementById('main');
+//a.style.color="#FF0000";
+var u=document.getElementsByClassName('title');
+u[0].style.color="green";
+u[0].style.fontWeight="bold";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
