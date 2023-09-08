4f964de2d42b41e1b5d0ecf8cc2cebe81d02a163
AmanRajVerma10 <verma.aman464@gmail.com>
2023-09-08 15:12:44 +0530

    thirdCommit

---
+++ thirdChange.js
@@ -0,0 +1,9 @@
+var a=document.getElementsByClassName('list-group-item');
+console.log(a);
+for(var i=0;i<a.length;i++)
+{
+    a[i].style.fontWeight="bold";
+}
+a[2].style.backgroundColor="green";
+
+
