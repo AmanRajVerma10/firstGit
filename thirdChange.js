862cae3a806056c0bf1047e6cffdb108e6e5fa0d
AmanRajVerma10 <verma.aman464@gmail.com>
2023-09-08 19:31:26 +0530

    7th

--- thirdChange.js
+++ thirdChange.js
@@ -1,11 +1,47 @@
-var a=document.querySelectorAll('.list-group-item');
-//console.log(a);
-a[1].style.color="green";
-var odd=document.querySelectorAll('li:nth-child(odd)');
-for(var i=0;i<odd.length;i++)
-{
-    odd[i].style.backgroundColor="green";
-}
+var i=document.querySelector('#items')
+//console.log(i.parentNode.parentNode.parentElement);
+//i.parentNode.style.backgroundColor="yellow";
+
+//console.log(i.previousElementSibling);
+
+var newDiv= document.createElement('div');
+ 
+newDiv.className='hey';
+
+newDiv.id='hi';
+
+newDiv.setAttribute('title','Hello');
+
+var text=document.createTextNode('HEllo');
+
+newDiv.appendChild(text);
+
+var container=document.querySelector('header .container');
+
+var h1=document.querySelector('header h1');
+//console.log(newDiv);
+
+container.insertBefore(newDiv,h1);
+
+var n= document.createElement('div');
+n.className='list-group';
+n.id='items';
+var t=document.createTextNode('HEllo');
+n.appendChild(t);
+var x=document.querySelector('.list-group')
+var y=document.querySelector('li');
+x.insertBefore(n,y);
+//console.log(n);
+
+
+
+
+
+
+
+
+
+
 
 
 
