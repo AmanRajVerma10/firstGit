d8373ecd4f930269844cef3776356ba9361cd8af
AmanRajVerma10 <verma.aman464@gmail.com>
2023-09-08 16:39:25 +0530

    sixth

--- thirdChange.js
+++ thirdChange.js
@@ -1,21 +1,14 @@
-//var a=document.getElementsByClassName('list-group-item');
+var a=document.querySelectorAll('.list-group-item');
 //console.log(a);
-//for(var i=0;i<a.length;i++)
-//{
-//    a[i].style.fontWeight="bold";
-//}
-//a[2].style.backgroundColor="green";
+a[1].style.color="green";
+var odd=document.querySelectorAll('li:nth-child(odd)');
+for(var i=0;i<odd.length;i++)
+{
+    odd[i].style.backgroundColor="green";
+}
 
-//var b=document.getElementsByTagName('Li');
-//console.log(b);
-//b[4].style.backgroundColor="yellow";
 
-//var i=document.querySelector('input[type="submit"]');
-//i.value='Chelsea'
- var item=document.querySelector('.list-group-item:nth-child(2)');
- item.style.backgroundColor="green";
- var i=document.querySelector('.list-group-item:nth-child(3)');
- i.style.color="white";
+
 
 
 
