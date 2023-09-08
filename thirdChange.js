bca62ee613d2f8ab10cf89fe68c056c8427e74fb
AmanRajVerma10 <verma.aman464@gmail.com>
2023-09-08 16:26:48 +0530

    fifthPush

--- thirdChange.js
+++ thirdChange.js
@@ -6,9 +6,22 @@
 //}
 //a[2].style.backgroundColor="green";
 
-var b=document.getElementsByTagName('Li');
-console.log(b);
-b[4].style.backgroundColor="yellow";
+//var b=document.getElementsByTagName('Li');
+//console.log(b);
+//b[4].style.backgroundColor="yellow";
+
+//var i=document.querySelector('input[type="submit"]');
+//i.value='Chelsea'
+ var item=document.querySelector('.list-group-item:nth-child(2)');
+ item.style.backgroundColor="green";
+ var i=document.querySelector('.list-group-item:nth-child(3)');
+ i.style.color="white";
+
+
+
+
+
+
 
 
 
