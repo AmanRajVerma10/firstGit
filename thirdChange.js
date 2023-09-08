ee1feac3df85adc23c91357cbe5247a34e502fc1
AmanRajVerma10 <verma.aman464@gmail.com>
2023-09-08 15:36:05 +0530

    fourthPush

--- thirdChange.js
+++ thirdChange.js
@@ -1,9 +1,16 @@
-var a=document.getElementsByClassName('list-group-item');
-console.log(a);
-for(var i=0;i<a.length;i++)
-{
-    a[i].style.fontWeight="bold";
-}
-a[2].style.backgroundColor="green";
+//var a=document.getElementsByClassName('list-group-item');
+//console.log(a);
+//for(var i=0;i<a.length;i++)
+//{
+//    a[i].style.fontWeight="bold";
+//}
+//a[2].style.backgroundColor="green";
+
+var b=document.getElementsByTagName('Li');
+console.log(b);
+b[4].style.backgroundColor="yellow";
+
+
+
 
 
